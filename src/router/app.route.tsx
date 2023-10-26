daf2458c5817a61b5bfdd65b6257c465eac69bb8
macbook <wkoua@oxylead.net>
2023-10-26 10:00:07 +0000

    feat : login logique implementation

--- src/router/app.route.tsx
+++ src/router/app.route.tsx
@@ -1,11 +1,13 @@
 import { useRoutes } from "react-router-dom";
 import App from "../pages/App";
 import Todo from "../pages/Todo/Todo";
+import Login from "src/pages/Login/Login";
 
 const AppRouting  = () => {
     let routes = useRoutes([
         { path: "/", element: <App /> },
         { path: "todo", element: <Todo /> },
+        { path: "login",element : <Login/>}
       ]);
       return routes;
 }
